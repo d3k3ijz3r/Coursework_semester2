5f4c1668e23a32e82f97ce943f89cc194dd1de6d
d3k3ijz3r <dekegijo@gmail.com>
2024-04-24 17:39:38 +0200

    Coursework semester 2

---
+++ operations.js
@@ -0,0 +1,7 @@
+console.log("Comparison Results:");
+console.log("5 > 3:", 5 > 3);
+console.log("5 < 3:", 5 < 3);
+console.log("5 === 3:", 5 === 3);
+console.log("5 !== 3:", 5 !== 3);
+console.log("5 <= 3:", 5 <= 3);
+console.log("5 >= 3:", 5 >= 3);
